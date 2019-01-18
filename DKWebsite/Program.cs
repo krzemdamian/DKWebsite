86f5c5dc0234f21cbee390ed2bc712bb2cab00bd
Damian Krzeminski <krzemdamian@gmail.com>
2019-01-18 17:19:02 +0100

    Jenkins test.

--- DKWebsite/Program.cs
+++ DKWebsite/Program.cs
@@ -19,6 +19,8 @@ namespace DKWebsite
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .UseUrls("http://*:5001");
     }
 }
